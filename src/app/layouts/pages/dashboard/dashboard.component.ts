38421585f3ab4455cfea91aadc168eddd46f5d6c
reconrez <reconrez@gmail.com>
2024-07-06 09:01:08 +0530

    added navigation

--- src/app/layouts/pages/dashboard/dashboard.component.ts
+++ src/app/layouts/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../shared/navbar/navbar.component";
+import { SidebarComponent } from "../../../shared/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
+    imports: [NavbarComponent, SidebarComponent]
 })
 export class DashboardComponent {
+  
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  darkMode : boolean = false;
+
+  getTitle = () => {
+    return "Connexio";
+  }
+
+  darkModeToggle(){
+    this.darkMode = !this.darkMode;
+    const body = document.body as HTMLElement
+    if(this.darkMode){
+      body.setAttribute('data-bs-theme', 'dark')
+    }else{
+      body.setAttribute('data-bs-theme', 'light')
+    }
+  }
+
 }
